93063062e58094e789352d763abdede771fc1195
lyrical590 <sammykibe31@gmail.com>
2025-09-09 03:15:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "lyrical-xmd",
+SESSION_ID: process.env.SESSION_ID || "pk~qUFz2BZJ#ozKJScL4uWMoP2Ad0c-WqQYFNvWt43ZOnpaV1BQaL_M",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
